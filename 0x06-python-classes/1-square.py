52f63cc1a03f6b08c4961fd537e56fb18721379f
kendimuthomi <kendijackline42@gmail.com>
2022-09-14 09:56:44 +0000

    Adding instance attribute to a class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Create a Square class'''
+
+
+class Square:
+    '''sqare class representation'''
+    def __init__(self, size)
+    '''initialize a new square class
+
+    Args:
+        size(integer): The size of the new square'''
+
+    self.__size = size
