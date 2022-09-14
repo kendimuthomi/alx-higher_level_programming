788be65af6167e568e952677593cf870e57bed7e
kendimuthomi <kendijackline42@gmail.com>
2022-09-14 03:30:28 +0000

    Creating an empty square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """create a class square"""
 class Square:
-    """This is a representation
-    of the class square"""
+    """representation of the class square"""
     pass # An empty block
