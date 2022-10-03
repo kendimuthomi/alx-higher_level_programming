db8a966ed3636d96496193c18fc264965f0e6f6e
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2022-10-03 20:36:18 +0300

    Almost a circle

--- 0x0C-python-almost_a_circle/tests/rectangle_test.py
+++ 0x0C-python-almost_a_circle/tests/rectangle_test.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Unit test class Rectangle"""
+import unittest
+import json
+import sys
+import os
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_output(self):
+        """Function to test the output the function rectangle"""
+
+        self.assertAlmostEqual(rectangle(,)
+
+    def test_rectangle_input(self):
+        """Function to test the input the function rectangle"""
+
+        self.assertRaises(TypeError, rectangle, )
