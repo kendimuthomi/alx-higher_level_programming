1daf3e49fb87454f53ba4394674540b80aa3612d
kendimuthomi <kendijackline42@gmail.com>
2022-11-29 07:32:45 -0800

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const n = Math.floor(Number(process.argv[2]));
+if (isNaN(n)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < n; i++) {
+    let r = '';
+    for (let j = 0; j < n; j++) {
+      r += 'X';
+    }
+    console.log(r);
+  }
+}
