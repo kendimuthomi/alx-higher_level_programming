a9c5e02542d1c600b2ede68a1a991a6998ccf45d
kendimuthomi <kendijackline42@gmail.com>
2022-12-04 08:19:38 +0000

    Relationships in sqlalchemy

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    cities = session.query(State, City).filter(\
+    cities = session.query(State, City).filter(
             City.state_id == State.id).all()
     for city in cities:
         print("{}: ({}) {}".format(city[0].name, city[1].id, city[1].name))
