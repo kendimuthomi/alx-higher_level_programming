d66c7d5bff4f7b58c8cf4a60a151cb2457f32b95
kendimuthomi <kendijackline42@gmail.com>
2022-12-04 08:17:48 +0000

    Relationships in sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+from model_city import City
+
+if __name__ == "__main__":
+    State.cities = relationship(
+                "City", order_by=City.id, back_populates="state")
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(State, City).filter(\
+            City.state_id == State.id).all()
+    for city in cities:
+        print("{}: ({}) {}".format(city[0].name, city[1].id, city[1].name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+class definition of a City and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from model_state import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
+
+class City(Base):
+    """
+    State class:
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, cant be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and cant be null
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
