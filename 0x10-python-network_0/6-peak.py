63fd3214b5306e445d02d6e818e65f592fc1d2ed
kendimuthomi <kendijackline42@gmail.com>
2022-12-15 19:36:56 +0000

    Finding the peak in a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""finding the peak in an array"""
+
+
+def find_peak(list_of_integers):
+    """finding the peak in a list"""
+    if list_of_integers == []:
+        return None
+    n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+    elif n == 2:
+        return max(list_of_integers)
+    for i in range(1, n - 1):
+        if (list_of_integers[i] >= list_of_integers[i - 1] and
+            list_of_integers[i] >= list_of_integers[i + 1]):
+                return list_of_integers[i]
