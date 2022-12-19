b65c128722f6331933e031bf06d29c3074c71a2f
kendimuthomi <kendijackline42@gmail.com>
2022-12-19 10:55:18 +0000

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """fetching a url using urllib"""
 import urllib.request
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    html = response.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
