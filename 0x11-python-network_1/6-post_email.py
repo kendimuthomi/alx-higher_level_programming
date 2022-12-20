c7ed838f3af18d9cd3836768be69c8d0ee158a36
kendimuthomi <kendijackline42@gmail.com>
2022-12-20 07:53:01 +0000

    POST

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email
+sends a POST request to the passed URL with the email as a parameter
+displays the body of the response (decoded in utf-8)"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    res = requests.post(url, data=value)
+    print(res.text)
