838cd89c4a944620e9ede90ed90f6fb7ef6cb4b9
kendimuthomi <kendijackline42@gmail.com>
2022-12-20 08:59:27 +0000

    need more research

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email
+sends a POST request to the passed URL with the email as a parameter
+displays the body of the response (decoded in utf-8)"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    value = {"q": letter}
+    response = requests.post(url, data=value)
+    try:
+        res = response.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("name"), res.get("id")))
+    except ValueError:
+        print("Not a valid JSON")
