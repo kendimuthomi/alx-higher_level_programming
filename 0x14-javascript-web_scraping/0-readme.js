3f975f9e6f1052252fd21413e3cc1d106cfff735
kendimuthomi <kendijackline42@gmail.com>
2023-01-19 07:57:50 +0000

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+
+const args = process.argv;
+const file = args[2];
+const fs = require('fs');
+
+fs.readFile(file, 'utf-8', (error, data) => {
+	if (error) {
+		console.error("Error" + error);
+	} else {
+		console.log(data);
+	}
+});
