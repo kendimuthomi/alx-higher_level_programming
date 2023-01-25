3714615ff46be98bade8357cbed2f8bd43ff4eea
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-01-25 13:22:05 +0300

    Task 0

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
 // Javascript script that updates the text color of the <header> element to red (#FF0000)
 
-const colorH = document.querySelector('header');
-colorH.style.color = '#FF0000';
+const headerColor = document.querySelector('header');
+headerColor.style.color = '#FF0000';
