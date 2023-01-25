b9b1edf5c2738d03221db6cce786d96ee1f12183
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-01-25 13:27:57 +0300

    Task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,5 @@
-// Javasript script
+// JavaScript script that fetches the character name
+
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+    $('DIV#character').html(data['name']);
+});
