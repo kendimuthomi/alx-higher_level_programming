df07cfeb102e1e7552c81e572958f11c8230b711
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-01-25 13:33:08 +0300

    Task 10

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,4 +1,4 @@
-// Javascript script that updates the text color of the <header> element to red (#FF0000)
-
-const headerColor = document.querySelector('header');
-headerColor.style.color = '#FF0000';
+// JavaScript script that updates the text color of the <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
